2ea8880238dd962e074b9181d6a093b0773cc847
zaqwes <igor.a.lugansky@gmail.com>
2019-10-06 16:23:58 +0300

    init kf

--- robust_kf.py
+++ robust_kf.py
@@ -24,14 +24,62 @@ if __name__ == '__main__':
             ipywidgets
         """
         from deps.filterpy.filterpy import kalman, common
-
+        from scipy.linalg import block_diag
         import numpy as np
         from kalman import KalmanFilter
         from common import Q_discrete_white_noise
 
         from deps.Kalman_and_Bayesian_Filters_in_Python_master.kf_book import book_plots
 
-        pass
+        from numpy.random import randn
+        import matplotlib.pyplot as plt
+        import numpy as np
+
+        # Gating
+        from deps.filterpy.filterpy.stats import mahalanobis
+
+
+        class PosSensor(object):
+            def __init__(self, pos=(0, 0), vel=(0, 0), noise_std=1.):
+                self.vel = vel
+
+                self.noise_std = noise_std
+                self.pos = [pos[0], pos[1]]
+
+            def read(self):
+                self.pos[0] += self.vel[0]
+
+                self.pos[1] += self.vel[1]
+                return [self.pos[0] + randn() * self.noise_std,
+                        self.pos[1] + randn() * self.noise_std]
+
+
+        pos, vel = (4, 3), (2, 1)
+        sensor = PosSensor(pos, vel, noise_std=1)
+        ps = np.array([sensor.read() for _ in range(50)])
+
+        # book_plots.plot_measurements(ps[:, 0], ps[:, 1]);
+        # plt.show()
+
+        z = ps
+        dt = 1.
+        F = np.array([[1, dt, 0, 0],
+                      [0, 1, 0, 0],
+                      [0, 0, 1, dt],
+                      [0, 0, 0, 1]])
+
+        q = Q_discrete_white_noise(dim=2, dt=dt, var=0.001)
+        Q = block_diag(q, q)
+
+        H = np.array([[1 / 0.3048, 0, 0, 0],
+                      [0, 0, 1 / 0.3048, 0]])
+
+        R = np.array([[5., 0],
+                      [0, 5]])
+        x = np.array([[0, 0, 0, 0]]).T
+        P = np.eye(4) * 500.
+
+
     elif key == 1:
         import subprocess
 
