4133a8dd4e15be9bdf26da63a5a6d1fcaf583a5a
zaqwes <igor.a.lugansky@gmail.com>
2019-11-30 13:53:21 +0300

    add first rnn

---
+++ rnn/time_seq_clustering.py
@@ -0,0 +1,5 @@
+# coding: utf-8
+
+# https://habr.com/ru/post/334220/
+# DTW
+# https://databricks.com/blog/2019/04/30/understanding-dynamic-time-warping.html
---
+++ rnn/top.py
@@ -0,0 +1,47 @@
+# coding: utf-8
+
+# https://stackoverflow.com/questions/1382925/virtualenv-no-site-packages-and-pip-still-finding-global-packages
+
+"""
+
+sudo apt install virtualenv
+virtualenv --no-site-packages -p python3.6 venv
+
+source venv/bin/activate
+
+# not pytorch
+pip install torch 
+#torchvision  # for images
+
+deactivate
+
+Examples:
+Time ser. pred:
+https://stackabuse.com/time-series-prediction-using-lstm-with-pytorch-in-python/
+
+# no loss fail + couple bugs in code
+https://gist.github.com/spro/ef26915065225df65c1187562eca7ec4
+
+# torch example
+https://github.com/pytorch/examples/tree/master/time_sequence_prediction
+
+# anoter
+https://towardsdatascience.com/lstm-for-time-series-prediction-de8aeb26f2ca
+
+Question LST multiple time series
+https://datascience.stackexchange.com/questions/23615/rnn-using-multiple-time-series
+(!!!) https://machinelearningmastery.com/multivariate-time-series-forecasting-lstms-keras/
+
+Multiple features:
+https://datascience.stackexchange.com/questions/17024/rnns-with-multiple-features
+
+Text:
+...
+
+"""
+
+def main():
+	pass
+
+if __name__ == '__main__':
+	main()
